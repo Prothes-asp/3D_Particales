2b5aa711b20bf35f52342a5b496a97688c1402a5
Prothes Barai <75435992+Prothes-asp@users.noreply.github.com>
2023-11-03 21:00:25 +0600

    Add files via upload

---
+++ all common file/js/analytics.js
@@ -0,0 +1,11 @@
+(function() {
+    var gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-0CWJCBYV82";
+    document.head.appendChild(gtagScript);
+    
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-0CWJCBYV82');
+  })();
